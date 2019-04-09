68ee82694e640f70cb0ac51d9ed2a2eb64d829fc
dhanyn10 <dhanyn.dhan@gmail.com>
2019-04-09 11:19:18 +0700

    menggunakan data dari konstruktor untuk dipanggil dalam program

---
+++ src/tes/java/oop2/Toko.java
@@ -0,0 +1,11 @@
+package tes.java.oop2;
+
+public class Toko {
+    public static void main(String[] args) {
+        Pelanggan p1 = new Pelanggan();
+        Pelanggan p2 = new Pelanggan();
+        
+        p1.perkenalan();
+        p2.perkenalan();
+    }
+}
