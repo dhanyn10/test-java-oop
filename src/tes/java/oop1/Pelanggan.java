009fd043613c09b707219ed7eb9b7596b73b3551
dhanyn10 <dhanyn.dhan@gmail.com>
2019-04-09 11:08:40 +0700

    pembaruan file folder

---
+++ src/tes/java/oop1/Pelanggan.java
@@ -0,0 +1,13 @@
+package tes.java.oop1;
+
+public class Pelanggan {
+    String nama;
+    int usia;
+    int tinggi;
+    
+    public void perkenalan()
+    {
+        System.out.println("perkenalkan, nama saya adalah " + nama);
+        System.out.println("usia mencapai " + usia + " dengan tinggi " + tinggi);
+    }
+}
