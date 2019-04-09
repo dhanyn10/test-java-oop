6f579847a11de179e76aca946719f13eb63db9fa
dhanyn10 <dhanyn.dhan@gmail.com>
2019-04-09 11:18:37 +0700

    memasukkan data hardcode melalui construktor

---
+++ src/tes/java/oop2/Pelanggan.java
@@ -0,0 +1,21 @@
+package tes.java.oop2;
+
+public class Pelanggan {
+    
+    String nama;
+    int usia;
+    int tinggi;
+    
+    public void perkenalan()
+    {
+        System.out.println("perkenalkan, nama saya adalah " + nama);
+        System.out.println("usia mencapai " + usia + " dengan tinggi " + tinggi);
+    }
+    
+    public Pelanggan()
+    {
+        nama    = "Arslan Senki";
+        usia    = 18;
+        tinggi  = 170;
+    }
+}
